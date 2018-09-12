7bc578ee1719291ca7584b3c4d15a228b6fa01e6
Frank Chung <ponty33@gmail.com>
2018-09-12 00:51:53 +0000

    Old-tweet section

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var maxLength = 140;
+  var count = $('#count')
+  $('textarea').keyup(function() {
+    var length = $(this).val().length;
+    var input = maxLength - length;
+    count.text(input);
+    if (input < 0) {
+      count.addClass('red');
+    } else {
+      count.removeClass('red');
+    }
+  })
+});
+
